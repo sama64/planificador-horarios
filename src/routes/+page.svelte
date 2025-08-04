9ca942c90e2752efaeb13169f37f064eedf93b00
sama <santiamaya88@gmail.com>
2025-08-04 00:28:10 -0300

    added contact info for reporting issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -63,6 +63,10 @@
           <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-5 w-5" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
           <div>
             <span class="font-semibold">Aviso:</span> Sistema en fase de desarrollo. Los resultados pueden contener errores.
+            <br>
+            <a href="https://wa.me/5491170600371" target="_blank" class="link link-primary text-xs">
+              Contacto para reportar errores
+            </a>
           </div>
         </div>
         
